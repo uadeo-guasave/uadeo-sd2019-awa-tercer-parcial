b6b16c9a643e9e7a39172084b3eabf7667d0b798
Bidkar Aragon Cardenas <bidkar@gmail.com>
2019-10-29 15:27:49 -0700

    Usuario MySQL

--- Models/MySQLDbContext.cs
+++ Models/MySQLDbContext.cs
@@ -6,7 +6,7 @@ namespace Segundo_Parcial.Models
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySQL("server=localhost;user=root;password=123;dbname=uadeo_tutorias;port=3306");
+            optionsBuilder.UseMySQL("server=localhost;user=admin_tutorias;password=321;dbname=uadeo_tutorias;port=3306");
             base.OnConfiguring(optionsBuilder);
         }
     }
